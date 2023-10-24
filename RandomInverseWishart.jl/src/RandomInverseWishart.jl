8d9454071857c3e5549ba40c3668cb02c48b79db
Seth Axen <seth@sethaxen.com>
2023-10-24 11:34:17 +0200

    Add Julia implementation of methods

---
+++ RandomInverseWishart.jl/src/RandomInverseWishart.jl
@@ -0,0 +1,117 @@
+module RandomInverseWishart
+
+using Distributions
+using LinearAlgebra
+using Random
+
+export rwishart_chol!, rinvwishart_chol!, rinvwishart_direct!, rinvwishart_indirect!
+
+function rwishart_chol!(rng::AbstractRNG, T::Matrix, m::Int, n::Int, UΣ::AbstractMatrix)
+    Z = T
+    for j in 1:m
+        for i in 1:(j - 1)
+            Z[i, j] = randn(rng)
+        end
+        Z[j, j] = rand(rng, Chi(n + 1 - j; check_args=false))
+        for i in (j + 1):m
+            Z[i, j] = 0
+        end
+    end
+    T = rmul!(Z, UpperTriangular(UΣ))
+    return T
+end
+
+function cholesky_upper!(
+    S,
+    _tmp::Union{Nothing,Matrix}=nothing,
+    _tmp2::Union{Nothing,Matrix}=nothing;
+    invert::Bool=false,
+    ischolU::Bool=false,
+)
+    if !invert
+        if ischolU
+            return UpperTriangular(S)
+        else
+            tmp = _tmp === nothing ? similar(S) : _tmp
+            return cholesky!(copyto!(tmp, S)).U
+        end
+    else
+        tmp = _tmp === nothing ? similar(S) : _tmp
+        tmp2 = _tmp2 === nothing ? similar(S) : _tmp2
+        if ischolU
+            copyto!(tmp2, S)
+            U = UpperTriangular(tmp2)
+        else
+            U = cholesky!(copyto!(tmp2, S)).U
+        end
+        C = LinearAlgebra.inv!(U)
+        mul!(tmp, triu!(parent(C)), C')
+        return cholesky!(tmp).U
+    end
+end
+
+function rinvwishart_chol!(rng::AbstractRNG, R::Matrix, m::Int, n::Int, UΩ::AbstractMatrix)
+    Z = R
+    for j in 1:m
+        for i in 1:(j - 1)
+            Z[i, j] = randn(rng)
+        end
+        Z[j, j] = rand(rng, Chi(n - m + j; check_args=false))
+    end
+    C = parent(LinearAlgebra.inv!(UpperTriangular(Z)))
+    triu!(C)
+    R = rmul!(C, UpperTriangular(UΩ))
+    return R
+end
+
+function rinvwishart_indirect!(
+    rng::AbstractRNG,
+    B::Matrix,
+    m::Int,
+    n::Int,
+    S::AbstractMatrix;
+    iscov::Bool=false,
+    ischolU::Bool=false,
+    retcholU::Bool=false,
+)
+    tmp = similar(B)
+    UΣ = cholesky_upper!(S, B, tmp; ischolU, invert=!iscov)
+    T = rwishart_chol!(rng, tmp, m, n, UΣ)
+    V = parent(LinearAlgebra.inv!(UpperTriangular(T)))
+    triu!(V)
+    mul!(B, V, UpperTriangular(V)')
+    if retcholU
+        R = triu!(cholesky!(B).factors)
+        return R
+    else
+        return B
+    end
+end
+
+function rinvwishart_direct!(
+    rng::AbstractRNG,
+    B::Matrix,
+    m::Int,
+    n::Int,
+    S::AbstractMatrix;
+    iscov::Bool=false,
+    ischolU::Bool=false,
+    retcholU::Bool=false,
+)
+    tmp1 = if ischolU && retcholU && !iscov
+        nothing
+    else
+        retcholU ? similar(B) : B
+    end
+    tmp2 = retcholU ? B : similar(B)
+    UΩ = cholesky_upper!(S, tmp1, tmp2; ischolU, invert=iscov)
+    R = rinvwishart_chol!(rng, tmp2, m, n, UΩ)
+    if retcholU
+        return R
+    else
+        mul!(B, UpperTriangular(R)', R)
+        return B
+    end
+end
+
+end  # module
---
+++ RandomInverseWishart.jl/test/runtests.jl
@@ -0,0 +1,87 @@
+using Distributions
+using LinearAlgebra
+using RandomInverseWishart
+using Random
+using Statistics
+using Test
+
+function rand_chol_pd_mat(rng, m)
+    R = qr(randn(rng, m, m)).R
+    R .*= sign.(diag(R))
+    return R
+end
+rand_chol_pd_mat(m) = rand_chol_pd_mat(Random.default_rng(), m)
+
+@testset "RandomInverseWishart.jl" begin
+    @testset "cholesky_upper!" begin
+        @testset for m in (5, 20)
+            UΩ = rand_chol_pd_mat(m)
+            Ω = UΩ' * UΩ
+            Σ = Matrix(inv(Symmetric(Ω)))
+            UΣ = Matrix(cholesky(Symmetric(Σ)).U)
+            @test RandomInverseWishart.cholesky_upper!(UΩ; invert=false, ischolU=true) ===
+                UpperTriangular(UΩ)
+            @test RandomInverseWishart.cholesky_upper!(Ω; invert=false, ischolU=false) ≈
+                UpperTriangular(UΩ)
+            @test RandomInverseWishart.cholesky_upper!(Σ; invert=true, ischolU=false) ≈
+                UpperTriangular(UΩ)
+            @test RandomInverseWishart.cholesky_upper!(UΣ; invert=true, ischolU=true) ≈
+                UpperTriangular(UΩ)
+        end
+    end
+
+    @testset "$rinvwishart!" for rinvwishart! in
+                                 (rinvwishart_indirect!, rinvwishart_direct!)
+        ndraws = 10_000
+        rng = Random.default_rng()
+        ntests = 4 * (5 * (5 + 1) + (20 * (20 + 1)))
+        α = 0.01 / ntests
+        atol_mul = quantile(Normal(), 1 - α / 2)
+        @testset for iscov in (true, false),
+            ischolU in (true, false),
+            m in (5, 20),
+            n in (m + 4, m + 10)
+
+            UΩ = rand_chol_pd_mat(m)
+            Ω = UΩ' * UΩ
+            if iscov
+                Σ = inv(cholesky(Symmetric(Ω)))
+                S = ischolU ? Matrix(cholesky(Symmetric(Σ)).U) : Matrix(Symmetric(Σ))
+            else
+                S = ischolU ? UΩ : Ω
+            end
+
+            B = similar(S)
+            rng2 = MersenneTwister(42)
+            @test @inferred(rinvwishart!(rng2, B, m, n, S; iscov, ischolU)) === B
+
+            UB = similar(B)
+            rng2 = MersenneTwister(42)
+            rinvwishart!(rng2, UB, m, n, S; iscov, ischolU, retcholU=true)
+            @test istriu(UB)
+            @test UB' * UB ≈ B
+
+            Bs = map(1:ndraws) do _
+                rinvwishart!(rng, similar(S), m, n, S; iscov, ischolU)
+            end
+            dist = InverseWishart(n, Ω)
+            mean_exp = mean(dist)
+            mean_est = mean(Bs)
+            # check marginal mean estimates consistent with known mean
+            for j in 1:m, i in 1:j
+                vij = var(dist, i, j)
+                @test mean_est[i, j] ≈ mean_exp[i, j] atol = atol_mul * sqrt(vij / ndraws)
+            end
+        end
+
+        if rinvwishart! === rinvwishart_direct!
+            m = n = 100
+            UΩ = rand_chol_pd_mat(m)
+            UB = similar(UΩ)
+            rinvwishart!(rng, UB, m, n, UΩ; iscov=false, ischolU=true, retcholU=true)
+            @test @allocated(
+                rinvwishart!(rng, UB, m, n, UΩ; iscov=false, ischolU=true, retcholU=true)
+            ) ≤ 160
+        end
+    end
+end
